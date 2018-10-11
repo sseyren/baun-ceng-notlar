01a2036ad47cbb0fb22ecbbaf11d5e619c76bc90
thesseyren <sseyren@hotmail.com>
2018-10-11 11:45:34 +0300

    11-10-2018 eklendi

---
+++ 11-10-2018/Kod8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Kod8
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+			if (BitConverter.IsLittleEndian)
+				Console.WriteLine("Little Endian - düşük anlamlı byte ilk sırada");
+			else
+				Console.WriteLine("Big Endian - yüksek anlamlı byte ilk sırada");
+
+			int a = 19785;
+			// 00000000 00000000 01001101 01001001
+
+			byte[] aByte = BitConverter.GetBytes(a);
+			foreach (byte i in aByte)
+			{
+				Console.Write(i + " ");
+			}
+			Console.WriteLine("\n" + BitConverter.ToInt32(aByte, 0));
+		}
+    }
+}
---
+++ 11-10-2018/Kod9/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Kod9
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+			// 00000001 00000010 00000011 00000010 00000001
+			byte[] kaynak = { 1, 2, 3, 2, 1 };
+
+			// 0000000000000000 0000000000000000 0000000000000000 0000000000000000
+			short[] hedef = new short[4];
+
+			// 0000001000000001 0000001000000011 0000000000000000 0000000000000000
+			Buffer.BlockCopy(kaynak, 0, hedef, 0, 4);
+			foreach (short i in hedef)
+			{
+				Console.Write("{0} ", i);
+			}
+
+			Console.WriteLine("\n\n");
+
+			short[] dizi1 = new short[4];
+			Console.WriteLine(Buffer.ByteLength(dizi1));
+
+			byte[] dizi2 = { 0, 1, 2, 3, 4, 5, 6 };
+			Console.WriteLine(Buffer.GetByte(dizi2, 5));
+
+			Buffer.SetByte(dizi2, 2, 112);
+			Console.WriteLine(dizi2[2]);
+		}
+    }
+}
