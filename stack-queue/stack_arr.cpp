acdb693440e8f28b1508c4d1e2970da327d985bd
thesseyren <sseyren@hotmail.com>
2018-10-29 00:57:05 +0300

    stack-queue: taslağa geçici fonksiyonlar dolduruldu

--- stack-queue/stack_arr.cpp
+++ stack-queue/stack_arr.cpp
@@ -3,13 +3,24 @@
 int array[2], size = 2, top = 0;
 
 int pop(){
-
+	return array[--top];
 }
 
 void push(int value){
+	array[top++] = value;
+}
 
+void look(){
+	for(int i = 0; i < top; i++){
+		printf("%d -> %d\n", i, array[i]);
+	}
 }
 
 int main(){
+	push(10);
+	push(20);
+	look();
+	printf("popped -> %d\n", pop());
+	printf("popped -> %d\n", pop());
 	return 0;
 }
