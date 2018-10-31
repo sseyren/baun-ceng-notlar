d43825e0bf680dca82576be3b7668c3a6dddd99c
thesseyren <sseyren@hotmail.com>
2018-10-31 12:23:13 +0300

    stack-queue: main kodu değiştirildi

--- stack-queue/main.cpp
+++ stack-queue/main.cpp
@@ -3,7 +3,41 @@
 #include "stack_ll.h"
 
 int main(){
-	printf("<---- Linked list ile stack örneği ---->\n");
+	printf("<--------- Array ile stack örneği --------->\n");
+
+	ar_stack* ar_stack1 = sa_init();
+	ar_stack* ar_stack2 = sa_init();
+
+	for (int i = 0; i < 10; i++)
+		sa_push(i*10, ar_stack1);
+	sa_look(ar_stack1);
+	for (int i = 0; i < 10; i++)
+		sa_push(sa_pop(ar_stack1), ar_stack2);
+	sa_look(ar_stack2);
+	sa_look(ar_stack1);
+
+	/*
+	int loopcount = 268435456;
+
+	int cursize = -1;
+	for(int i = 0; i < loopcount; i++){
+		sa_push(i, ar_stack1);
+		if(sa_size(ar_stack1) != cursize){
+			cursize = sa_size(ar_stack1);
+			printf("current size -> %d\n", cursize);
+		}
+	}
+
+	for(int i = 0; i < loopcount; i++){
+		sa_pop(ar_stack1);
+		if(sa_size(ar_stack1) != cursize){
+			cursize = sa_size(ar_stack1);
+			printf("current size -> %d\n", cursize);
+		}
+	}
+	*/
+
+	printf("\n\n<--------- Linked list ile stack örneği --------->\n");
 
 	node* ll_stack1 = sl_init();
 
@@ -25,46 +59,22 @@ int main(){
 	printf("popped -> %d\n", sl_pop(&ll_stack1));
 	sl_look(ll_stack1);
 
-	printf("%d\n", sizeof(node));
+	sl_push(1, &ll_stack1);
+	sl_push(2, &ll_stack1);
+	sl_push(3, &ll_stack1);
+	sl_look(ll_stack1);
+	printf("popped -> %d\n", sl_pop(&ll_stack1));
+	printf("popped -> %d\n", sl_pop(&ll_stack1));
+	printf("popped -> %d\n", sl_pop(&ll_stack1));
+	sl_look(ll_stack1);
 
+	/*
 	for(int i = 0; i < 67108864; i++)
 		sl_push(i, &ll_stack1);
 
 	for(int i = 0; i < 67108864; i++)
 		sl_pop(&ll_stack1);
-
-
-	printf("<---- Array ile stack örneği ---->\n");
-
-	ar_stack* ar_stack1 = sa_init();
-	ar_stack* ar_stack2 = sa_init();
-
-	for (int i = 0; i < 10; i++)
-		sa_push(i*10, ar_stack1);
-	sa_look(ar_stack1);
-	for (int i = 0; i < 10; i++)
-		sa_push(sa_pop(ar_stack1), ar_stack2);
-	sa_look(ar_stack2);
-	sa_look(ar_stack1);
-
-	int loopcount = 268435456;
-
-	int cursize = -1;
-	for(int i = 0; i < loopcount; i++){
-		sa_push(i, ar_stack1);
-		if(sa_size(ar_stack1) != cursize){
-			cursize = sa_size(ar_stack1);
-			printf("current size -> %d\n", cursize);
-		}
-	}
-
-	for(int i = 0; i < loopcount; i++){
-		sa_pop(ar_stack1);
-		if(sa_size(ar_stack1) != cursize){
-			cursize = sa_size(ar_stack1);
-			printf("current size -> %d\n", cursize);
-		}
-	}
+	*/
 
 	return 0;
 }
