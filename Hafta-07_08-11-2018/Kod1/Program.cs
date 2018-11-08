e1572314e028a26d605fe9224330a861f3e6daa6
thesseyren <sseyren@hotmail.com>
2018-11-08 10:15:25 +0300

    Hafta 7: örnekler eklendi

---
+++ Hafta-07_08-11-2018/Kod1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Kod1
+{
+    class Hayvanlar
+    {
+        protected double Boy, Ağırlık;
+        public Hayvanlar(double boy, double ağırlık)
+        {
+            this.Boy = boy;
+            this.Ağırlık = ağırlık;
+        }
+        public void ÖzellikGöster()
+        {
+            Console.WriteLine("Boy = " + Boy);
+            Console.WriteLine("Ağırlık = " + Ağırlık);
+        }
+    }
+    class Kedi : Hayvanlar
+    {
+        public string Türü;
+        // Yapıcı metod
+        // base(boy,ağırlık) ile çağırılan aslında Hayvanlar sınıfının yapıcı 
+        //   metodudur.
+        public Kedi(string türü, int boy, int ağırlık) : base(boy, ağırlık)
+        {
+            this.Türü = türü;
+        }
+        public void TürGöster()
+        {
+            Console.WriteLine(Türü + " Kedisi");
+        }
+    }
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Kedi kedi1 = new Kedi("Van", 5, 10);
+            Kedi kedi2;
+
+            // normal atama
+            kedi2 = kedi1;
+
+            kedi2.ÖzellikGöster();
+            kedi2.TürGöster();
+            Console.WriteLine();
+
+            Hayvanlar hayvan1 = new Hayvanlar(6, 11);
+            hayvan1.ÖzellikGöster();
+            Console.WriteLine();
+
+            // Türeyen sınıfa ilişkin bir referans temel sınıfa ilişkin bir
+            //   referansa atanabilir.
+            hayvan1 = kedi1;
+            hayvan1.ÖzellikGöster();
+
+            // Hatalı kullanım: hayvan1.TürGöster();
+        }
+    }
+}
---
+++ Hafta-07_08-11-2018/Kod2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Kod2
+{
+    class Hayvanlar
+    {
+        protected double mBoy, mAğırlık;
+        public Hayvanlar(double boy, double ağırlık)
+        {
+            this.mBoy = boy;
+            this.mAğırlık = ağırlık;
+        }
+        public void ÖzellikGöster()
+        {
+            Console.WriteLine("Boy = " + mBoy);
+            Console.WriteLine("Ağırlık = " + mAğırlık);
+        }
+        public double Boy
+        {
+            get { return mBoy; }
+            set { mBoy = value; }
+        }
+        public double Ağırlık
+        {
+            get { return mAğırlık; }
+            set { mAğırlık = value; }
+        }
+    }
+    class Kedi : Hayvanlar
+    {
+        public string Türü;
+        public Kedi(string türü, int boy, int ağırlık) : base(boy, ağırlık)
+        {
+            this.Türü = türü;
+        }
+        public void TürGöster()
+        {
+            Console.WriteLine(Türü + " Kedisi");
+        }
+    }
+    class Koyun : Hayvanlar
+    {
+        public string Türü;
+        public Koyun(string türü, int boy, int ağırlık) : base(boy, ağırlık)
+        {
+            this.Türü = türü;
+        }
+        public void TürGöster()
+        {
+            Console.WriteLine(Türü + " Koyunu");
+        }
+    }
+    class MainClass
+    {
+        public static void Göster(Hayvanlar hayvan)
+        {
+            Console.WriteLine("Boy = " + hayvan.Boy);
+            Console.WriteLine("Ağırlık = " + hayvan.Ağırlık);
+            Console.WriteLine();
+        }
+        public static void Main(string[] args)
+        {
+            Hayvanlar hayvan1 = new Hayvanlar(10, 35);
+            Göster(hayvan1);
+
+            Kedi kedi1 = new Kedi("Van", 5, 10);
+            Göster(kedi1);
+
+            Koyun koyun1 = new Koyun("Ankara", 52, 80);
+            Göster(koyun1);
+        }
+    }
+}
