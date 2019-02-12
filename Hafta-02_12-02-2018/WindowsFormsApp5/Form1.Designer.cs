ba0134697b651a11e8762aa89d7b1113a1923903
thesseyren <sseyren@hotmail.com>
2019-02-12 14:11:44 +0300

    Hafta 2: örnek eklendi

---
+++ Hafta-02_12-02-2018/WindowsFormsApp5/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace WindowsFormsApp5
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ArrayList";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "ListBox";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 70);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Arama";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 99);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Fonksiyon";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 128);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Prosedür";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(12, 157);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "`out` ifadesi";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(116, 128);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(100, 23);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "Temizle";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(116, 72);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(116, 101);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(330, 228);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Hafta-02_12-02-2018/WindowsFormsApp5/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] Sayi = new int[] { 10, 22, 33, 44 };
+            int[] Sayi2 = new int[] { 3, 5, 8, 11 };
+            string[] a = new string[] { "Ahmet", "Mehmet" };
+            ArrayList liste = new ArrayList();
+
+            liste.Add(Sayi);
+            liste.Add(Sayi2);
+            liste.Add(a);
+            liste.Add(55);
+            liste.Add("Balikesir");
+
+            MessageBox.Show( ( (int[])liste[0] )[1].ToString() );
+            MessageBox.Show( ( (int[])liste[1] )[2].ToString() );
+            MessageBox.Show( ( (string[])liste[2] )[0].ToString() );
+            MessageBox.Show(liste[3].ToString());
+            MessageBox.Show(liste[4].ToString());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ListBox Bilgi = new ListBox();
+            Bilgi.Items.Add("Ankara");
+            Bilgi.Items.Add("İstanbul");
+            Bilgi.Items.Add("Bursa");
+            Bilgi.Items.Add("İzmir");
+            Bilgi.Items.Add("Balıkesir");
+
+            MessageBox.Show(Bilgi.Items[1].ToString());
+
+            ArrayList liste = new ArrayList();
+            liste.AddRange(Bilgi.Items);
+            MessageBox.Show(liste[2].ToString());
+
+            int y = liste.Capacity;
+            MessageBox.Show(y.ToString());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ListBox Bilgi = new ListBox();
+            Bilgi.Items.Add("Ankara");
+            Bilgi.Items.Add("İstanbul");
+            Bilgi.Items.Add("Bursa");
+            Bilgi.Items.Add("İzmir");
+            Bilgi.Items.Add("Balıkesir");
+
+            string icerik = textBox1.Text;
+            if (Bilgi.Items.Contains(icerik) == true)
+                MessageBox.Show("Kayıt var!");
+            else
+                MessageBox.Show("Kayıt yok...");
+        }
+        private string YeniDeger(string Deger)
+        {
+            string yazi = "Merhaba " + Deger;
+            return yazi;
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(YeniDeger(textBox2.Text));
+        }
+        private void Parametre(int s1, int s2)
+        {
+            int sonuc = s1 + s2;
+            MessageBox.Show(sonuc.ToString());
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Parametre(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text));
+        }
+        private void Degerlerimiz(out int s1, out int s2)
+        {
+            s1 = Convert.ToInt32(textBox1.Text);
+            s2 = Convert.ToInt32(textBox2.Text);
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Degerlerimiz(out int sayi1, out int sayi2);
+            MessageBox.Show("Sayı1: " + sayi1 + "\nSayı2: " + sayi2);
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+        }
+    }
+}
