be8b4301ad7d11ba4fdc53dd3af0c6fbe10b44d2
thesseyren <sseyren@hotmail.com>
2019-03-12 14:18:37 +0300

    Hafta 6: Örnekler eklendi

---
+++ Hafta-06_12-03-2019/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            checkedListBox1.Items.Add(textBox1.Text);
+            textBox1.Text = string.Empty;
+            textBox1.Focus();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            while (checkedListBox1.CheckedItems.Count > 0)
+                checkedListBox1.Items.Remove(checkedListBox1.CheckedItems[0]);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (checkedListBox1.CheckedItems.Count > 0)
+            {
+                string secimler = string.Empty;
+                for (int i = 0; i < checkedListBox1.CheckedItems.Count; i++)
+                    secimler += (i + 1).ToString() + ". " + checkedListBox1.CheckedItems[i].ToString() + "\n";
+                MessageBox.Show(secimler);
+            }
+            else
+                MessageBox.Show("Hiç seçim yapılmadı.");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            bool tumunuSec = true;
+            for (int i = 0; i < checkedListBox1.Items.Count; i++)
+                checkedListBox1.SetItemChecked(i, tumunuSec);
+        }
+    }
+}
