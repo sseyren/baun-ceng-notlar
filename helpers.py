1d09d39bc45388888ce987f7fc7c0ea88aa7eb2f
Serhat Seyren <sseyren@hotmail.com>
2021-01-31 18:49:06 +0300

    manhattan_distance added

--- helpers.py
+++ helpers.py
@@ -2,3 +2,6 @@ import numpy as np
 
 def euclidean_distance(first:np.ndarray, second:np.ndarray):
     return np.sqrt(np.sum((first-second)**2))
+
+def manhattan_distance(first:np.ndarray, second:np.ndarray):
+    return np.sum(np.abs(first-second))
