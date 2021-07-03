2d4814fbfbfd54d640be1e790f7efd14406ce9c2
Serhat Seyren <sseyren@hotmail.com>
2021-07-03 23:29:15 +0300

    gcd modular inverse

--- gcd_euclidean.py
+++ gcd_euclidean.py
@@ -27,9 +27,7 @@ def main(a:int, b:int, pdf_path=Path.cwd() / __file__[:-3]):
 
     doc = _get_latex_doc()
     doc.append(pylatex.NoEscape(
-        r"\textbf{Soru:} " +
-        f"$ebob({a}, {b})$ kaçtır? " +
-        f"$ebob({a}, {b}) = {a}x + {b}y$ ifadesinde $x$ ve $y$ sayıları ne olur?"
+        r"\textbf{Soru:} " + f"$ebob({a}, {b})$ kaçtır?"
     ))
 
     doc.append(pylatex.NoEscape(f"""
@@ -55,13 +53,48 @@ def main(a:int, b:int, pdf_path=Path.cwd() / __file__[:-3]):
 
     doc.append(pylatex.NoEscape(f"Sonuç olarak $ebob({a}, {b}) = {gcd}$ olur."))
 
+    doc.append(pylatex.NoEscape(
+        r"\textbf{Soru:} " +
+        f"$ebob({a}, {b}) = {a}x + {b}y$ ifadesinde $x$ ve $y$ sayıları ne olur?"
+    ))
+
     x, y = _components_of_gcd(a, b, gcd)
+
+    doc.append(pylatex.NoEscape(f"""
+        Öklid adımları tersten takip edilerek bu değerler elde edilebilir:
+    """))
+
     doc.append(pylatex.NoEscape(f"""
         $ebob({a}, {b}) = {gcd} = {a} \\cdot {x} + {b} \\cdot {y}$ olduğundan,
         $x = {x}$ ve $y = {y}$ olacaktır.
     """))
 
+    if gcd == 1:
+        doc.append(pylatex.NoEscape(
+            r"\textbf{Soru:} " +
+            f"${b}$ sayısının $mod$ {a} için aritmetik tersi nedir?"
+        ))
+
+        doc.append(pylatex.NoEscape(r"""
+            $a \ne 0$ herhangi bir tamsayı olmak üzere, eğer
+            $a \cdot a^* \equiv 1 \, mod \, n$ denkliğini sağlayan bir $a^*$
+            tamsayısı var ise bu $a^*$ sayısına $a$’nın $mod \, n$’ye göre
+            aritmetik tersi denir. Eğer $ebob(a, n) = 1$ ise (yani $a$ ve $n$
+            aralarında asal ise) $a$’nın $mod \, n$’ye göre aritmetik tersi
+            \underline{vardır}.
+        """))
+
+        doc.append(pylatex.NoEscape(f"""
+            ${gcd} = {a} \\cdot {x} + {b} \\cdot {y}$ olduğundan
+            ${b} \\cdot {y} \\equiv 1 \\, mod \\, {a}$ yazılabilir.
+        """))
+
+        doc.append(pylatex.NoEscape(f"""
+            Böylece, ${b}$ sayısının $mod$ {a} için aritmetik tersi ${y}$
+            bulunur.
+        """))
+
     doc.generate_pdf(pdf_path)
 
 if __name__ == "__main__":
-    main(1547, 560)
+    main(513, 7)
